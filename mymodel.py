354376971f834177c72ddc15d1ede3025e40234d
dylant93 <39739012+dylant93@users.noreply.github.com>
2019-05-22 21:21:00 +0800

    Update mymodel.py

--- mymodel.py
+++ mymodel.py
@@ -7,9 +7,7 @@ Created on Wed Jan 16 00:25:08 2019
 
 import os
 import time
-#import logging
 import tensorflow as tf
-#from cnn_image_classifier.image_loading import read_img_sets
 from image_loading import read_img_sets
 import numpy as np
 import cv2
@@ -74,40 +72,22 @@ def new_fully_connected_layer(layer, num_inputs, num_outputs, use_relu=True, lay
     if use_relu:
         layer = tf.nn.relu(layer)
 
-    #if summaries: 
-        #tf.summary.histogram("Weight_fc" + str(layer_id), weights)
-        #tf.summary.histogram("bias_fc" + str(layer_id), biases)
-
     return layer
-
-"""
-def log_progress(session, saver, cost, accuracy, epoch, test_feed_dict, checkpoint_path):
-
-    val_loss = session.run(cost, feed_dict=test_feed_dict)
-    acc = session.run(accuracy, feed_dict=test_feed_dict)
-
-    msg = "Epoch {0} --- Accuracy: {1:>6.1%}, Validation Loss: {2:.3f}"
-    logging.info(msg.format(epoch, acc, val_loss))
-
-    save_path = saver.save(session, checkpoint_path)
-    logging.debug("Creating resource: CNN Model [%s]", save_path)
-"""
 
 def variables(flat_img_size, num_classes):
 
     with tf.name_scope('input'):
         x = tf.placeholder(tf.float32, shape=[None, flat_img_size], name='x-input')
         y_true = tf.placeholder(tf.float32, shape=[None, num_classes], name='y_true')
-        #keep_prob = tf.placeholder(tf.float32)
 
-    return x, y_true#, keep_prob
+    return x, y_true
 
 
 def model(x, img_size, colour_channels, filter_size, neurons, num_classes):
 
     with tf.name_scope('reshaping'):
         x_image = tf.reshape(x, [-1, img_size, img_size, colour_channels])
-        #tf.summary.image('example_images', x_image)
+
 
     with tf.name_scope('Conv1'):
         layer_conv1 = new_conv_layer(
@@ -145,9 +125,7 @@ def model(x, img_size, colour_channels, filter_size, neurons, num_classes):
             #summaries=True
         )
 
-#    with tf.name_scope('Dropout'):
 
-#       dropout_layer = dropout(layer_fc1, keep_prob)
 
     with tf.name_scope('Fully_Connected2'):
 
@@ -157,7 +135,7 @@ def model(x, img_size, colour_channels, filter_size, neurons, num_classes):
             num_outputs=num_classes,
             use_relu=False,
             layer_id=2,
-            #summaries=True
+            
         )
 
     return layer_fc2
@@ -170,36 +148,6 @@ def softmax(logits):
 
     return y_pred
 
-
-
-"""
-def optimizer(cost):
-
-    with tf.name_scope('train'):
-        training_op = tf.train.AdamOptimizer(learning_rate=1e-4).minimize(cost)
-
-    return training_op
-"""
-
-"""
-
-def restore_or_initialize(session, saver, checkpoint_dir):
-
-    ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
-    
-
-    if ckpt:
-        logging.debug("Loading resource: CNN Model [%s]", os.path.join(checkpoint_dir, 'model.ckpt'))
-        saver.restore(session, ckpt.model_checkpoint_path)
-
-    else:
-        logging.warning(
-            "Resource not found: CNN Model [%s]. Model will now be trained from scratch.",
-            os.path.join(checkpoint_dir, 'model.ckpt'))
-
-        os.makedirs(checkpoint_dir)
-        tf.global_variables_initializer().run()
-"""
 
 def predict(img_dir, model_dir, img_size=64, colour_channels=3, batch_size=803):
 
@@ -224,7 +172,6 @@ def predict(img_dir, model_dir, img_size=64, colour_channels=3, batch_size=803):
     with tf.Session() as sess:
         
         saver = tf.train.Saver()
-        #restore_or_initialize(sess, saver, checkpoint_dir)
         
         writer = tf.summary.FileWriter(log_dir, graph=tf.get_default_graph()) 
         writer.add_graph(tf.get_default_graph()) 
@@ -234,33 +181,16 @@ def predict(img_dir, model_dir, img_size=64, colour_channels=3, batch_size=803):
         x_predict_batch, y_predict_batch, idss, cls_predict_batch = data.train.next_batch(batch_size=803)
         
         x_predict_batch = x_predict_batch.reshape(batch_size, flat_img_size)
-        print(idss[0],idss[1])
-        print(x_predict_batch)
-        testimg = cv2.imread('test.png')
-        testimg = cv2.resize(testimg, (64,64), cv2.INTER_LINEAR)
-        #img=img.astype(np.float32)
-        testbin = np.empty((1,12288),np.float32)
-        #testbin=np.vstack((testbin,testimg.flatten()))
-        testbin[0]=testimg.flatten()
-        testbin=np.multiply(testbin, 1.0/255.0)
-        print(testbin)
-        #print(testimg.flatten().shape)
+
         start = time.time()
         prediction = sess.run([tf.argmax(predict_op, dimension=1)], feed_dict={x: x_predict_batch})
-        #prediction = sess.run([tf.argmax(predict_op, dimension=1)], feed_dict={x: testbin})
-        
-        #print(prediction[0],cls_predict_batch)
-		
-	  #if category_ref[prediction[0][0]] == cls_predict_batch[0]:
-        #    print("it is correct")
-	
        
         end = time.time()
         timepershot=(end-start)/batch_size
         
         
         idsspath=img_dir + '/predict/'+cls_predict_batch[0]+'/'+idss[0]
-        
+        #CV2 part
         font                   = cv2.FONT_HERSHEY_SIMPLEX
         topleftCornerOfText = (10,40)
         topleftCornerOfText2 = (10,70)
